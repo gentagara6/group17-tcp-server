42554661fa5e07abbcdca088c384d3d32a5267bf
gentagara6 <genta.gara@student.uni-pr.edu>
2026-04-14 16:30:03 +0200

    shtimi i list dhe read

--- Server/FileManager.cs
+++ Server/FileManager.cs
@@ -1,17 +1,33 @@
 using System;
 using System.IO;
+using System.Linq;
 
-public class FileManager
-{
+public class FileManager {
     private readonly string folderPath;
 
-    public FileManager()
-    {
+    public FileManager() {
         folderPath = "server_files";
 
         if (!Directory.Exists(folderPath))
         {
             Directory.CreateDirectory(folderPath);
         }
+    }
+
+    public string ListFiles() {
+    var files = Directory.GetFiles(folderPath);
+
+    return string.Join("\n", files);
+    }
+
+    public string ReadFile(string fileName) {
+        string path = Path.Combine(folderPath, fileName);
+
+        if (!File.Exists(path))
+        {
+            return "File not found";
+        }
+
+        return File.ReadAllText(path);
     }
 }
