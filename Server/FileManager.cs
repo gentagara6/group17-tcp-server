17d5fd7945548858b6f570876780b90fbbac9566
gentagara6 <genta.gara@student.uni-pr.edu>
2026-04-15 13:40:43 +0200

    shtimi i funksioneve write, download, delete

--- Server/FileManager.cs
+++ Server/FileManager.cs
@@ -8,26 +8,51 @@ public class FileManager {
     public FileManager() {
         folderPath = "server_files";
 
-        if (!Directory.Exists(folderPath))
-        {
+        if (!Directory.Exists(folderPath)) {
             Directory.CreateDirectory(folderPath);
         }
     }
 
     public string ListFiles() {
-    var files = Directory.GetFiles(folderPath);
+        var files = Directory.GetFiles(folderPath);
 
-    return string.Join("\n", files);
+        return string.Join("\n", files.Select(Path.GetFileName));    
     }
 
     public string ReadFile(string fileName) {
         string path = Path.Combine(folderPath, fileName);
 
-        if (!File.Exists(path))
-        {
-            return "File not found";
+        if (!File.Exists(path)) {
+            return "Fajlli nuk u gjet";
         }
-
         return File.ReadAllText(path);
     }
+
+    public string WriteFile(string fileName, string content) {
+        string path = Path.Combine(folderPath, fileName);
+
+        File.WriteAllText(path, content);
+        return "Fajlli u shkrua me sukses";
+    }
+
+    public byte[] DownloadFile(string fileName) {
+        string path = Path.Combine(folderPath, fileName);
+
+        if (!File.Exists(path)) {
+            return null;
+        }
+        return File.ReadAllBytes(path);
+    }
+
+    public string DeleteFile(string fileName) {
+        string path = Path.Combine(folderPath, fileName);
+
+        if (!File.Exists(path)) {
+            return "Fajlli nuk u gjet";
+        }
+
+        File.Delete(path);
+        return "Fajlli u fshi me sukses";
+    }
+
 }
